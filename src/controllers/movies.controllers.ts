68b73a0d8d0e1397605926545b6743ac5367e4ef
jessica <jessica_02_06@outlook.com>
2023-02-27 11:09:14 -0300

    fix: tipagem funçoes controllers

--- src/controllers/movies.controllers.ts
+++ src/controllers/movies.controllers.ts
@@ -6,7 +6,7 @@ import { readMoviesService } from "../services/readMovies.services"
 import updateMovieService from "../services/updateMovie.services"
 
 
-const createMovieController = async ( req: Request, res: Response) => {
+const createMovieController = async ( req: Request, res: Response): Promise<Response> => {
     const movieData: iMovieCreate = req.body
 
     const newMovie = await createMoviesService(movieData)
@@ -14,26 +14,26 @@ const createMovieController = async ( req: Request, res: Response) => {
     return res.status(201).json(newMovie)
 }
 
-const readMoviesController = async ( req: Request, res: Response) => {
+const readMoviesController = async ( req: Request, res: Response): Promise<Response> => {
     const { perPage, page } = req.query
     
     const movies = await readMoviesService(
         perPage,
-        page,
+        page
     )
 
     return res.status(200).json(movies)
 
 }
 
-const deleteMovieController = async ( req: Request, res: Response) => {
+const deleteMovieController = async ( req: Request, res: Response): Promise<Response> => {
 
     await deleteMovieService(parseInt(req.params.id))
 
     return res.status(204).send()
 }
 
-const updateMovieController = async ( req: Request, res: Response) =>{
+const updateMovieController = async ( req: Request, res: Response): Promise<Response> =>{
     const movieData = req.body
     const idMovie = parseInt(req.params.id)
 
